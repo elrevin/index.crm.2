796da5afdfaebf92206924cbef5154c3cfe34c57
elrevin <elrevin@gmail.com>
2017-10-13 09:51:16 +0500

    The ButterKnife was added, and was created first login screen

--- app/src/main/java/me/elrevin/indexcrm/ui/LoginActivity.java
+++ app/src/main/java/me/elrevin/indexcrm/ui/LoginActivity.java
@@ -4,25 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.elrevin.indexcrm.Constants;
 import me.elrevin.indexcrm.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.btnGo)
+    Button btnGo;
+
+    @BindView(R.id.etLogin)
+    EditText etLogin;
+
+    @BindView(R.id.etPassword)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
-    public void btnOkClick(View v) {
+    @OnClick(R.id.btnGo)
+    public void btnGoClick(View v) {
+        Toast.makeText(this, etLogin.getText().toString(), Toast.LENGTH_LONG).show();
         setResult(Constants.RESULT_OK);
-        finish();
-    }
-
-    public void btnCancelClick(View v) {
-        setResult(Constants.RESULT_CANCEL);
         finish();
     }
 }
